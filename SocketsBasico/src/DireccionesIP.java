de6aa0b6099201480ae1d80312f025f600d7bc45
Felipe Rosero <feliperosero@live.com>
2015-04-21 11:40:32 -0500

    Más ejemplos de InetAddress DireccionesIP (Bien explicado) "identidades"

--- SocketsBasico/src/DireccionesIP.java
+++ SocketsBasico/src/DireccionesIP.java
@@ -16,7 +16,23 @@ public class DireccionesIP {
           out.println();
           
           //OBTENEMOS EL OBJETO EQUIPO A PARTIR DEL NOMBRE
+          String host = "www.google.com";
+          equipo = InetAddress.getByName(host); //devuelve un objeto InetAddress(dirección IP) a patir de un dominio tipo String 
+          out.println("el equipo "+ host + "es: " + equipo );
+          out.println("Su dirección IP es: " + equipo.getHostAddress()); //regresa un string de la representación decimal de la dirección IP
+          out.println("Su nombre es: " + equipo.getHostName());//regresa un string equivalente al nombre de dominio de la dirección IP
+          out.println("Y su nombre canónico: " + equipo.getCanonicalHostName()); //se obtiene el nombre completo del host correspondiente a una dirección IP. 
+          out.println();
           
+          //OBTENEMOS EL EQUIPO A PARTIR DE SU DIRECCIÓN IP
+          equipo = InetAddress.getByAddress(direccionLocal);
+          out.println("Mi equipo es: " + equipo );
+          out.println("Su dirección IP es: " + equipo.getHostAddress());
+          out.println();
+          
+          //OBTENEMOS TODAS LAS DIRECCIONES IP DE UN EQUIPO
+          InetAddress[] identidades; // Declaración del objeto (como poner una etiqueta al objeto)
+          identidades = InetAddress.getAllByName(equipo.getHostName());
           
       } //Fin try
   }  //Fin de public static void main(String[] args)
