140ad52817fb8d537081588c078eb990d349ca24
Felipe Rosero <feliperosero@live.com>
2015-04-22 15:06:40 -0500

    Inicio TCPRchoServerjava

--- SocketsBasico/src/ClienteServidor/TCPEchoServer.java
+++ SocketsBasico/src/ClienteServidor/TCPEchoServer.java
@@ -14,18 +14,27 @@ public class TCPEchoServer {
         int servPort = Integer.parseInt(args[0]);
         
         //CREACIÓN DE UN SOCKET DE SERVIDOR PARA ACEPTAR SOLICITUDES DE CONEXIÓN AL CLIENTE
-        ServerSocket servSock = new ServerSocket(servPort); //Creación de un objeto tió ServerSocket.
+        ServerSocket servSock = new ServerSocket(servPort); //Creación de un objeto tipo ServerSocket. //servSock escucha las solicitudes de conexión de cliente en el puerto especificado en el constructor.
         
         int recvMsgSize;
-        byte[] receoveBuf = new byte[BUFSIZE]; //Recibe búfer.
+        byte[] receiveBuf = new byte[BUFSIZE]; //Recibe búfer.
         
         while(true) {   //Correr para siempre, la aceptación y el servicio de conexiones
-            Socket clntSock = servSock.accept(); //Obtiene, acepta o abre la conexión al cliente.
+            Socket clntSock = servSock.accept(); //Obtiene, acepta o abre la conexión al cliente. //Cuando se establece una conexión esta bloquea cualquier otra conexión, el resto de conexiones quedan en cola.
             
-            SocketAddress clientAddress = clntSock.getRemoteSocketAddress(); //Obtiene la dirección del cliente
+            SocketAddress clientAddress = clntSock.getRemoteSocketAddress(); //Obtiene la dirección del cliente //
             out.println("Manejo de cliente en " + clientAddress);
             
+            InputStream in = clntSock.getInputStream();
+            OutputStream out = clntSock.getOutputStream();
+            
+            //RECIBE HASTA QUE EL CLIENTE CIERRA LA CONEXIÓN, ESTO SE INDICA CUANDO RETORNA -1
+            while ((recvMsgSize = in.read(receiveBuf)) != -1) {
+                out.write(receiveBuf,0,recvMsgSize);
+            }
+            clntSock.close(); //Cierre del Socket 
+            
         }
-        
+        //no alcanza
     }
 }
