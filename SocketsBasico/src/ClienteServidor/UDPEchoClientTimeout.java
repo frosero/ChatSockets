cc762864d24e3fe1155bde90d942807671c5ede8
Felipe Rosero <feliperosero@live.com>
2015-04-22 19:34:12 -0500

    Implementación de parámetros en TCP y UDP

--- SocketsBasico/src/ClienteServidor/UDPEchoClientTimeout.java
+++ SocketsBasico/src/ClienteServidor/UDPEchoClientTimeout.java
@@ -8,51 +8,53 @@ import java.io.InterruptedIOException;
 
 public class UDPEchoClientTimeout {
 
-  private static final int TIMEOUT = 3000;   // Resend timeout (milliseconds)
-  private static final int MAXTRIES = 5;     // Maximum retransmissions
+  private static final int TIMEOUT = 3000;   // Tiempo de espera para cada reenvío (milisegundos).
+  private static final int MAXTRIES = 5;     // Maximo de retransmisiones.
 
   public static void main(String[] args) throws IOException {
 
-    if ((args.length < 2) || (args.length > 3)) { // Test for correct # of args
-      throw new IllegalArgumentException("Parameter(s): <Server> <Word> [<Port>]");
+    if ((args.length < 2) || (args.length > 3)) { // Prueba de número de argumentos correctos
+      throw new IllegalArgumentException("Parámetros(s): <Servidor> <Palabra> [<Puerto>]");
     }
-    InetAddress serverAddress = InetAddress.getByName(args[0]);  // Server address
-    // Convert the argument String to bytes using the default encoding
+    InetAddress serverAddress = InetAddress.getByName(args[0]);  // Dirección del servidor
+    
+
+    // CONVERTIR EL ARGUMENTO STRING EN BYTES UTILIZANDO LA CODIFICACIÓN POR DEFECTO
     byte[] bytesToSend = args[1].getBytes();
 
     int servPort = (args.length == 3) ? Integer.parseInt(args[2]) : 7;
 
     DatagramSocket socket = new DatagramSocket();
 
-    socket.setSoTimeout(TIMEOUT);  // Maximum receive blocking time (milliseconds)
+    socket.setSoTimeout(TIMEOUT);  // Máxima tiempo de bloqueo para recibir (milisegundos).
 
-    DatagramPacket sendPacket = new DatagramPacket(bytesToSend,  // Sending packet
+    DatagramPacket sendPacket = new DatagramPacket(bytesToSend,  // Envio de paquetes
         bytesToSend.length, serverAddress, servPort);
 
-    DatagramPacket receivePacket =                              // Receiving packet
+    DatagramPacket receivePacket =                              // Recepción de paquetes
         new DatagramPacket(new byte[bytesToSend.length], bytesToSend.length);
 
-    int tries = 0;      // Packets may be lost, so we have to keep trying
+    int tries = 0;      // Los paquetes se pueden perder, entonces se sigue intentando
     boolean receivedResponse = false;
     do {
-      socket.send(sendPacket);          // Send the echo string
+      socket.send(sendPacket);          // Envía la cadena ECO (Echo)
       try {
-        socket.receive(receivePacket);  // Attempt echo reply reception
+        socket.receive(receivePacket);  // intento de recepción por parte de la cadena ECO (Echo)
 
-        if (!receivePacket.getAddress().equals(serverAddress)) {// Check source
-          throw new IOException("Received packet from an unknown source");
+        if (!receivePacket.getAddress().equals(serverAddress)) {// Comprobar Fuente
+          throw new IOException("Se recibió un paquete de una fuente desconocida");
 	}
         receivedResponse = true;
-      } catch (InterruptedIOException e) {  // We did not get anything
+      } catch (InterruptedIOException e) {  // Excepción "Cuando no se a conceguido nada"
         tries += 1;
-        System.out.println("Timed out, " + (MAXTRIES - tries) + " more tries...");
+        System.out.println("Tiempo agotado, " + (MAXTRIES - tries) + " intento(s) más...");
       }
     } while ((!receivedResponse) && (tries < MAXTRIES));
 
     if (receivedResponse) {
-      System.out.println("Received: " + new String(receivePacket.getData()));
+      System.out.println("Recibido: " + new String(receivePacket.getData()));
     } else {
-      System.out.println("No response -- giving up.");
+      System.out.println("No responde -- darse por vencido.");
     }
     socket.close();
   }
