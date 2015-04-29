8a0b491c4c2ed6d766b2d609d141082f5ec6b5f0
Felipe Rosero <feliperosero@live.com>
2015-04-29 17:21:21 -0500

    framer (Completo)

--- SaRSockets/src/sarsockets/Framer.java
+++ SaRSockets/src/sarsockets/Framer.java
@@ -4,7 +4,7 @@ package sarsockets;
 import java.io.IOException;
 import java.io.OutputStream;
 
-
+//Una interface sirve para establecer un 'protocolo' entre clases. 
 public interface Framer {
   void frameMsg(byte[] message, OutputStream out) throws IOException;
   byte[] nextMsg() throws IOException;
