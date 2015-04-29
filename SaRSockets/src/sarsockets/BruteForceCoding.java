aa336bf1dd58d078affdf11cea60e1302e8fbb51
Felipe Rosero <feliperosero@live.com>
2015-04-29 13:45:43 -0500

    BruteForceCoding (decodificación de datos)

--- SaRSockets/src/sarsockets/BruteForceCoding.java
+++ SaRSockets/src/sarsockets/BruteForceCoding.java
@@ -1,6 +1,6 @@
 
 package sarsockets;
-
+import static java.lang.System.*;
 public class BruteForceCoding {
     //ELEMENTO DE DATOS PARA CODIFICARD 
     //Nota: variables byte, short, int y long son enteros primitivos
@@ -17,7 +17,7 @@ public class BruteForceCoding {
     private final static int ISIZE = Integer.SIZE / Byte.SIZE;
     private final static int LSIZE = Long.SIZE / Byte.SIZE;
     
-    private final static int BYTEMASK = 0XFF; //8bits //fitratemos los 8 primeros bits por medio de la máscara para cada uno de los enteros primitivos
+    private final static int BYTEMASK = 0XFF; //8bits //fitratemos los 8 primeros bytes(32 bytes en total) por medio de la máscara para cada uno de los enteros primitivos
                                               /* (Arquitectura de SW nivel bits)
                                                 byte = 1 byte
                                                 short = 2 bytes
@@ -27,6 +27,53 @@ public class BruteForceCoding {
                                                 double = 8 bytes
                                                 char = 2 bytes
                                                 bolean = 1 byte
+                                                BYTEMASK mantiene el valor del byte de ser extendido a firmar cuando se convierte a un int en la llamada a append (). Lo cual lo hace como un entero sin signo
+                                                bytemask Borra los bits más altos - esencialmente negando el signo solicitado implícitamente durante la conversión a la representación de cadena decimal del valor binario.
                                                 */
+    //IMPRIME CADA BYTE DE LA MATRIZ DADA COMO UN VALOR DECIMAL SIN SIGNO
+    
+    public static String byteArrayToDecimalString(byte[] bArray){
+        StringBuilder rtn = new StringBuilder(); //declaro objeto del tipo StringBuilder
+        for (byte b : bArray){
+            //Conversión Decimal
+            rtn.append(b & BYTEMASK).append(" "); //setText reemplaza el texto antes escrito, append añade el texto luego de lo que ya está escrito.
+           
+        }
+        return rtn.toString();        
+    }
+    // Advertencia: Condiciones previas no provadas (EJ: 0<= size <=8)
+    public static int encodeIntBigEndian(byte[] dst, long val, int offset, int size){ //Iterar sobre bytes de tamaño de la matriz dada, acumulando el resultado en un long, que se desplaza a la izquierda con cada interación
+        for (int i = 0; i < size; i++){
+            dst[offset++] = (byte) (val >> ((size - i -1) * Byte.SIZE)); //Operaciones con bytes >> recorre estacios en el numero binario val
+                                                                          //El reparto (byte) descarta los bit más altos, esto equivale a una operación de desplazamiento y máscara.
+        }
+        return offset;
+        
+    }
+    //Advertencia: COndiciones previas no probadas (EJ: 0 <= size <=8)
+    public static long decodeIntBigEndian(byte[] val, int offset, int size){//Iterar sobre bytes de tamaño de la matriz dada, acumulando el resultado en un long, que se desplaza a la izquierda con cada interación
+        long rtn = 0;
+        for(int i = 0; i < size; i++){
+            rtn = (rtn << Byte.SIZE)| ((long) val[offset + i] & BYTEMASK);
+        }
+        return rtn;        
+    }
+    
+    //Demostrar métidos aplicados
+    public static void main(String[] args){
+        byte[] message = new byte[BSIZE + SSIZE + ISIZE + LSIZE]; //Preparar matriz para recibir la serie de números enteros
+        //CODIFICAR LOS CAMPOS DE LA MATRIZ (ARREGLO) DE BYTES DE DESTINO
+        //Nota:  El byte, short, int y long se codifica en la matriz en la secuencia descrita anterior. 
+        int offset = encodeIntBigEndian(message, byteVal, 0, BSIZE); //Parametrizar objeto offset
+        offset = encodeIntBigEndian(message, shortVal, offset, SSIZE);
+        offset = encodeIntBigEndian(message, intVal, offset, SSIZE);
+        encodeIntBigEndian(message, longVal, offset, LSIZE);
+        out.println("Mensaje decodificado: " + byteArrayToDecimalString(message));
+        
+        //DECODIFICAR VARIOS CAMPOS
+        
+    }
+    
+    
     
 }
