da2b3816517d97ef77700ae23877063a0dd991e5
Felipe Rosero <feliperosero@live.com>
2015-04-29 11:03:41 -0500

    BruteForceCoding (1era parte)

---
+++ SaRSockets/src/sarsockets/BruteForceCoding.java
@@ -0,0 +1,13 @@
+
+package sarsockets;
+
+public class BruteForceCoding {
+    //ELEMENTO DE DATOS PARA CODIFICAR
+    private static byte byteVal = 101; // cientouno
+    private static short shortVal = 10001; //Diez mil uno
+    private static int intVal = 100000001; //Cien millones y uno
+    private static long longVal = 1000000000001L; //Un trillon y uno //Recordar L indica que se trata de un tipo "long"
+    
+    private final static int BSIZE = Byte.SIZE / Byte.SIZE; //El método "SIZE" simplemente retorna el tamaño del arreglo o número de elementos
+    
+}
