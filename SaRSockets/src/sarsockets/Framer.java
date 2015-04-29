bfca2c44b8d423d3146c9f4f3c361d725d0a9741
Felipe Rosero <feliperosero@live.com>
2015-04-29 18:02:56 -0500

    Longitud del mensaje

--- SaRSockets/src/sarsockets/Framer.java
+++ SaRSockets/src/sarsockets/Framer.java
@@ -7,6 +7,7 @@ import java.io.OutputStream;
 //Una interface sirve para establecer un 'protocolo' entre clases. 
 public interface Framer {
   void frameMsg(byte[] message, OutputStream out) throws IOException;
+  
   byte[] nextMsg() throws IOException;
     
 }
