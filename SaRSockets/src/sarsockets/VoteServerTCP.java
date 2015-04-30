ff358fe43a631e89c91fbf69fbf7877109c1591f
Felipe Rosero <feliperosero@live.com>
2015-04-30 14:29:41 -0500

    VoteServerTCP

---
+++ SaRSockets/src/sarsockets/VoteServerTCP.java
@@ -0,0 +1,17 @@
+package sarsockets;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class VoteServerTCP {
+    
+      public static void main(String args[]) throws Exception {
+              if (args.length != 1) { // Prueba de número de argumentos correctos
+      throw new IllegalArgumentException("Parameter(s): <Port>");
+    }
+          
+          
+      }
+    
+}
