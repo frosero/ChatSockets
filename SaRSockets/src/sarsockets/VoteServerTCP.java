69d555cf3b9519b0ad72dc6f2d7cae039f8e39df
Felipe Rosero <feliperosero@live.com>
2015-04-30 15:00:54 -0500

    VoteServerTCP (Completo)

--- SaRSockets/src/sarsockets/VoteServerTCP.java
+++ SaRSockets/src/sarsockets/VoteServerTCP.java
@@ -1,5 +1,5 @@
 package sarsockets;
-
+import static java.lang.System.*;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,8 +8,37 @@ public class VoteServerTCP {
     
       public static void main(String args[]) throws Exception {
               if (args.length != 1) { // Prueba de número de argumentos correctos
-      throw new IllegalArgumentException("Parameter(s): <Port>");
+      throw new IllegalArgumentException("Parámetro(s): <Puerto>");
     }
+              int port = Integer.parseInt(args[0]); // Se guarda el número de puerto en la varaible port
+              ServerSocket servSock = new ServerSocket(port); //Objeto con el atributo port 
+              
+//CAMBIAR DE TEXTO A BINARIO SOBRE LAS DIREFENTES CODIFICACIONES DEL CLIENTE/SERVIDOR (ESTRATEGIA DE ENCUADRE)
+              VoteMsgCoder coder = new VoteMsgBinCoder();                               //CLASE VoteMsgCoder
+              VoteService service = new VoteService();                                  //CLASE VoteService
+              
+              while (true) {
+               Socket clntSock = servSock.accept();
+               out.println("Manejo de cliente en" +clntSock.getLocalSocketAddress());
+               //Cambiar Longitud de DELIM por una estrategia diferente encuadre
+               Framer framer = new LengthFramer(clntSock.getInputStream());             //CLASE Framer //CLASE LengthFramer
+               try{
+                   byte[] req;
+                   while ((req = framer.nextMsg()) != null) {
+                       out.println("Mensaje recibido (" + req.length + " bytes)");
+                       VoteMsg responseMsg = service.handleRequest(coder.fromWire(req)); //CLASE VoteMsg
+                       framer.frameMsg(coder.toWire(responseMsg), clntSock.getOutputStream());
+                   }
+                  
+              }catch (IOException ioe){
+                  err.println("Error manejo del cliente: " + ioe.getMessage());
+              } finally {
+                   out.println("Cerrando Conexión");
+                   clntSock.close();
+               }
+                  
+                  
+              }
           
           
       }
