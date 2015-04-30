c524442cb05dfd85a214c9a979cb85b17790bca6
Felipe Rosero <feliperosero@live.com>
2015-04-29 19:06:01 -0500

    Interface del mensaje de voto

--- SaRSockets/src/sarsockets/VoteMsgCoder.java
+++ SaRSockets/src/sarsockets/VoteMsgCoder.java
@@ -3,6 +3,10 @@ package sarsockets;
 
 import java.io.IOException;
 public interface VoteMsgCoder {
+    /*
+     El método toWire () convierte el mensaje de voto a una secuencia de bytes de acuerdo con un particular,
+    protocolo, y la fromWire () método analiza una secuencia dada de bytes de acuerdo con el mismo
+    protocolo y construye una instancia de la clase de mensaje. */
     
     byte[] toWire(VoteMsg msg) throws IOException;
     
