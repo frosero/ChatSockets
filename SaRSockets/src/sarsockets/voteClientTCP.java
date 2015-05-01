7c0a91aafcc4cd8be62a29b007f00c54017d936e
Felipe Rosero <feliperosero@live.com>
2015-04-30 19:40:38 -0500

    VoteClienteTCP

---
+++ SaRSockets/src/sarsockets/voteClientTCP.java
@@ -0,0 +1,13 @@
+package sarsockets;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class voteClientTCP {
+    public static final int CANDIDATEID = 888;
+    public static void main(String args[]) throws Exception {
+        if (args.length != 2) { // Prueba si el número argumentos es correcto
+      throw new IllegalArgumentException("Parameter(s): <Server> <Port>");
+    }
+        
+    }
+}
