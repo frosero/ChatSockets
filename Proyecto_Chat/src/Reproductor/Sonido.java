1def4ce77b3703ad5772b1b7a3cf4af633877752
Felipe Rosero <feliperosero@live.com>
2015-05-09 00:48:10 -0500

    Revert ""
    This reverts commit 000b778c5e2059a4b1055b48711aad87b3f3166b.

--- Proyecto_Chat/src/Reproductor/Sonido.java
+++ Proyecto_Chat/src/Reproductor/Sonido.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package Reproductor;
-
 import javax.swing.*;
 import javax.sound.sampled.*;
 import java.io.*;
@@ -12,22 +11,27 @@ import java.io.*;
 /**
  *
  */
-public class Sonido extends JFrame {
+public class Sonido extends JFrame{
 
-    public Sonido() {
 
-        File sf = new File("Grabacion.wav");
+    
+    public Sonido()
+{
 
-        AudioFileFormat aff;
-        AudioInputStream ais;
+File sf=new File("Grabacion.wav");
 
-        try {
+AudioFileFormat aff;
+AudioInputStream ais;
+
+try
+{
 //obtiene el tipo de archivo
-            aff = AudioSystem.getAudioFileFormat(sf);
+aff=AudioSystem.getAudioFileFormat(sf);
 //Obtiene un flujo de entrada de audio desde el proporcionado File
-            ais = AudioSystem.getAudioInputStream(sf);
+ais=AudioSystem.getAudioInputStream(sf);
 
-            AudioFormat af = aff.getFormat();
+
+AudioFormat af=aff.getFormat();
 
 //Construye una línea de datos de información objeto de la información especificada, 
 //que incluye un único formato de audio y un tamaño de búfer deseado. Este constructor se utiliza 
@@ -36,31 +40,39 @@ public class Sonido extends JFrame {
 //lineClass - la clase de la línea de datos descrito por el objeto info
 //format formato deseado -
 //bufferSize - deseada tamaño del buffer en bytes
-            DataLine.Info info = new DataLine.Info(
-                    Clip.class,
-                    ais.getFormat(),
-                    ((int) ais.getFrameLength() * af.getFrameSize()));
+DataLine.Info info = new DataLine.Info(
+Clip.class,
+ais.getFormat(),
+((int) ais.getFrameLength() * af.getFrameSize()));
 //El Clip interfaz representa un tipo especial de línea de datos cuyos datos de audio se pueden cargar 
 //antes de la reproducción, en lugar de ser transmitido en tiempo real.
-            Clip ol = (Clip) AudioSystem.getLine(info);
+Clip ol = (Clip) AudioSystem.getLine(info);
 
-            ol.open(ais);
-            ol.start();
+ol.open(ais);
+ol.start();
 //para el numero de veces que se reproduce
-            ol.loop(0);
+ol.loop(0);
 
-            System.out.println("reprodución empezada :)");
-            Thread.sleep(20000);
-        } catch (UnsupportedAudioFileException ee) {
+System.out.println("reprodución empezada :)");
+Thread.sleep(20000);
+}
+catch(UnsupportedAudioFileException ee){
+ 
+}
+catch(IOException ea){
+     
+}
+catch(LineUnavailableException LUE){
+   
+}       
+catch (InterruptedException ex) {
+            
+}
 
-        } catch (IOException ea) {
-
-        } catch (LineUnavailableException LUE) {
-
-        } catch (InterruptedException ex) {
-
-        }
-
+}
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Sonido();
     }
-
+    
 }
